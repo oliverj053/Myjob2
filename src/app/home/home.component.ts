142af48ed9d256b9020eb11d5d11a9d2102ba644
AndresRyes01 <gusanitorela@gmail.com>
2018-12-04 02:01:30 -0600

    Home
    Se añade diseño al home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,8 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
+export class HomeComponent implements OnInit {
+  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
   constructor() { }
 
   ngOnInit() {
