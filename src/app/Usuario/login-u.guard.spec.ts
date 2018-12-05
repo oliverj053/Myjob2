69003b4e1acca2c1757658a8b56c3aadf2d1687a
TirandoCodigoF <virgo_santiago@live.com.mx>
2018-12-04 21:43:18 -0600

    LOL

--- src/app/Usuario/login-u.guard.spec.ts
+++ src/app/Usuario/login-u.guard.spec.ts
@@ -3,7 +3,11 @@ import { TestBed, async, inject } from '@angular/core/testing';
 import { LoginUGuard } from './login-u.guard';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 describe('LoginEGuard', () => {
+=======
+describe('LoginUGuard', () => {
+>>>>>>> parent of fcd0520... Merge branch 'master' of https://github.com/oliverj053/Myjob2
 =======
 describe('LoginUGuard', () => {
 >>>>>>> parent of fcd0520... Merge branch 'master' of https://github.com/oliverj053/Myjob2
--- src/app/Usuario/nologin-u.guard.spec.ts
+++ src/app/Usuario/nologin-u.guard.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed, async, inject } from '@angular/core/testing';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { NologinEGuard } from './nologin-u.guard';
 
 describe('NologinEGuard', () => {
@@ -12,6 +13,8 @@ describe('NologinEGuard', () => {
 
   it('should ...', inject([NologinEGuard], (guard: NologinEGuard) => {
 =======
+=======
+>>>>>>> parent of fcd0520... Merge branch 'master' of https://github.com/oliverj053/Myjob2
 import { NologinUGuard } from './nologin-u.guard';
 
 describe('NologinUGuard', () => {
@@ -22,6 +25,9 @@ describe('NologinUGuard', () => {
   });
 
   it('should ...', inject([NologinUGuard], (guard: NologinUGuard) => {
+<<<<<<< HEAD
+>>>>>>> parent of fcd0520... Merge branch 'master' of https://github.com/oliverj053/Myjob2
+=======
 >>>>>>> parent of fcd0520... Merge branch 'master' of https://github.com/oliverj053/Myjob2
     expect(guard).toBeTruthy();
   }));
