4947fbbdb4dfdc511c9e40be718fde9bafc46bde
oliverj053 <oliver_js597@outlook.com>
2018-12-12 12:58:58 -0600

    campos validados
    sistema funcional con validaciones

--- src/app/Usuario/registro-u/registro-u.component.ts
+++ src/app/Usuario/registro-u/registro-u.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 //servicio
 import {EmpleadoService} from '../servicios/empleado.service';
 //
-import { NgForm } from '@angular/forms';
+import { NgForm,FormBuilder, FormGroup, Validators} from '@angular/forms';
+
 //clase empresa
 import { Usuario } from '../modelos/usuario';
 
@@ -13,11 +14,28 @@ import { Usuario } from '../modelos/usuario';
 })
 export class RegistroUComponent implements OnInit {
 
-  constructor(private empleadoService: EmpleadoService) { }
+  formEmpresa: FormGroup;
+
+  constructor(private empleadoService: EmpleadoService,private fb: FormBuilder) {  this.buildForm();}
 
   ngOnInit() {
     this.empleadoService.getProducts();
     this.limpiarFormulario();
+  }
+
+  buildForm() {
+    this.formEmpresa = this.fb.group({
+      nombre: ['', Validators.compose([Validators.required,Validators.minLength(4), Validators.maxLength(15), Validators.pattern('^[A-Z]{1}[A-Z-a-z]*')]) ],
+      apellidos: ['', Validators.compose([Validators.required,Validators.minLength(4), Validators.maxLength(15), Validators.pattern('^[A-Z]{1}[A-Z-a-z]*')])] ,
+      rfc: ['', Validators.compose([Validators.required,Validators.minLength(13), Validators.maxLength(13),Validators.pattern('^[A-Z]{4}[0-9]{6}[A-Z0-9]{3}')])],
+      correo: ['', Validators.compose([Validators.required,Validators.email]) ],
+      contrasenia: ['', Validators.compose([Validators.required,Validators.minLength(4), Validators.maxLength(10)])],
+      telefono: ['', Validators.compose([Validators.required,Validators.minLength(10), Validators.maxLength(10),Validators.pattern('[0-9]{10}')]) ],
+      direccion: ['', Validators.compose([Validators.required,Validators.minLength(5),Validators.maxLength(10)])],
+      profesion: ['', Validators.compose([Validators.required,Validators.minLength(6),Validators.maxLength(20)]) ],
+      gradoEstudio: ['', Validators.compose([Validators.required,Validators.minLength(6),Validators.maxLength(20)]) ],
+      
+    });
   }
 
   onSubmit(productForm: NgForm){
